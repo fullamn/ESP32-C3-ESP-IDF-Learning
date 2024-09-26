5ed719cc80caa43dae733a24836548ec6e0f1425
nfullam <nfullam@bitome.com>
2024-09-26 15:17:47 -0400

    add config to enable motor blinking add config to set gpio pins used for the motor add functions to setup motor pins and blink motor add randomized colors to led strip blink

--- motor_blink/main/blink_example_main.c
+++ motor_blink/main/blink_example_main.c
@@ -13,6 +13,7 @@
 #include "esp_log.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
+#include "esp_random.h"
 
 static const char *TAG = "example";
 
@@ -22,6 +23,8 @@ static const char *TAG = "example";
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
 static uint8_t s_led_state = 0;
+static uint8_t s_motor_state = 0;
+static uint8_t s_motor_high_pin = 0;
 
 #ifdef CONFIG_BLINK_LED_STRIP
 
@@ -32,7 +35,14 @@ static void blink_led(void)
     /* If the addressable LED is enabled */
     if (s_led_state) {
         /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        /* Randomize the color by splitting the rand uint32_t into 4 uint8_t */
+        uint32_t *colors[4];
+        uint32_t rng = esp_random();
+        colors[0] = (rng & 0x000000ff);
+        colors[1] = (rng & 0x0000ff00)>>8;
+        colors[2] = (rng & 0x00ff0000)>>16;
+        colors[4] = (rng & 0xff000000)>>24;
+        led_strip_set_pixel(led_strip, 0, colors[0],colors[1],colors[2]);
         /* Refresh the strip to send data */
         led_strip_refresh(led_strip);
     } else {
@@ -40,7 +50,6 @@ static void blink_led(void)
         led_strip_clear(led_strip);
     }
 }
-
 static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink addressable LED!");
@@ -88,11 +97,32 @@ static void configure_led(void)
 #error "unsupported LED type"
 #endif
 
+#ifdef CONFIG_BLINK_MOTOR_INCLUDE
+
+#define MOTOR_PINS(pos) (pos? CONFIG_BLINK_MOTOR_GPIO_2:CONFIG_BLINK_MOTOR_GPIO_1)
+
+static void configure_motor(void){
+    ESP_LOGI(TAG,"Configured to 'blink' motor");
+    gpio_reset_pin(MOTOR_PINS(true));
+    gpio_reset_pin(MOTOR_PINS(false));
+    gpio_set_direction(MOTOR_PINS(true), GPIO_MODE_OUTPUT);
+    gpio_set_direction(MOTOR_PINS(false), GPIO_MODE_OUTPUT);
+}
+
+static void blink_motor(void){
+    gpio_set_level(MOTOR_PINS(s_motor_high_pin), s_motor_state);
+}
+#else
+    static void configure_motor(void){};
+    static void blink_motor(void){};
+#endif
+
 void app_main(void)
 {
 
     /* Configure the peripheral according to the LED type */
     configure_led();
+    configure_motor();
 
     while (1) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
@@ -100,5 +130,12 @@ void app_main(void)
         /* Toggle the LED state */
         s_led_state = !s_led_state;
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Turning the Motor %s: %s!", s_motor_high_pin == true ? "FWD" : "BWD", s_motor_state == true ? "ON": "OFF");
+        blink_motor();
+        s_motor_state = !s_motor_state;
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        if(s_motor_state){
+            s_motor_high_pin = !s_motor_high_pin;
+        }
     }
 }
