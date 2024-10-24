073324583b48ad69a06335091f9cc68d43eaf970
nfullam <nfullam@bitome.com>
2024-10-24 13:09:32 -0400

    find speed ranges for each direction and calibrate directional duty cycles.

--- mechanics-base/main/mechanics_base_main.c
+++ mechanics-base/main/mechanics_base_main.c
@@ -8,41 +8,66 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "driver/sdm.h"
+#include "driver/gpio.h"
 
-#define EXAMPLE_SIGMA_DELTA_GPIO_NUM 0
-#define EXAMPLE_LED_DIM_PERIOD_MS    1000
-#define EXAMPLE_LED_DIM_DELAY_MS     10
-#define EXAMPLE_LED_DIM_DUTY_STEP    2
-#define EXAMPLE_LED_DIM_DUTY_MAX     90
-#define EXAMPLE_LED_DIM_DUTY_MIN     (EXAMPLE_LED_DIM_DUTY_MAX - EXAMPLE_LED_DIM_PERIOD_MS / EXAMPLE_LED_DIM_DELAY_MS * EXAMPLE_LED_DIM_DUTY_STEP)
+#include "sdkconfig.h"
 
-static const char *TAG = "sdm_led";
+#define DIR_GPIO_NUM CONFIG_MECHANICS_DIR_GPIO_NUM
+#define SPD_GPIO_NUM CONFIG_MECHANICS_SDM_GPIO_NUM
+#define SDM_DUTY_MAX(dir) (dir? INT8_MIN:INT8_MAX) 
+#define SDM_DUTY_MIN(dir) (dir? -110:87)
+#define SDM_DUTY_OFF(dir) (dir? INT8_MAX:INT8_MIN)
+
+
+static const char *TAG = "mechanics_base";
+static uint8_t motor_dir = 0;
+
+int8_t calculate_sdm_density(int8_t dir, int8_t duty){
+    int8_t density = (int8_t)((SDM_DUTY_MAX(dir)-SDM_DUTY_MIN(dir))*(duty/100.0)+SDM_DUTY_MIN(dir));
+    return density;
+}
+
+int8_t get_default_sdm_density(int8_t dir){
+    return calculate_sdm_density(dir, CONFIG_MECHANICS_DEFAULT_SDM_DUTY);
+}
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Install sigma delta channel");
+
+    ESP_LOGI(TAG, "Set up direction GPIO");
+    gpio_reset_pin(DIR_GPIO_NUM);
+    gpio_set_direction(DIR_GPIO_NUM, GPIO_MODE_OUTPUT);
+    gpio_set_pull_mode(DIR_GPIO_NUM, GPIO_PULLDOWN_ONLY);
+    gpio_set_level(DIR_GPIO_NUM, motor_dir);
+
+    ESP_LOGI(TAG, "Install speed sigma delta channel");
     sdm_channel_handle_t sdm_chan = NULL;
     sdm_config_t config = {
         .clk_src = SDM_CLK_SRC_DEFAULT,
-        .gpio_num = EXAMPLE_SIGMA_DELTA_GPIO_NUM,
+        .gpio_num = SPD_GPIO_NUM,
         .sample_rate_hz = 1 * 1000 * 1000, // 1MHz sample rate
     };
     ESP_ERROR_CHECK(sdm_new_channel(&config, &sdm_chan));
 
     ESP_LOGI(TAG, "Enable sigma delta channel");
     ESP_ERROR_CHECK(sdm_channel_enable(sdm_chan));
+    ESP_ERROR_CHECK(sdm_channel_set_pulse_density(sdm_chan, INT8_MIN)); // INT_MIN is a zero value for the pulse
 
-    ESP_LOGI(TAG, "Change duty cycle continuously");
-    int8_t duty = 0;
-    int step = EXAMPLE_LED_DIM_DUTY_STEP;
-    while (1) {
-        ESP_ERROR_CHECK(sdm_channel_set_pulse_density(sdm_chan, duty));
-        /* By changing delay time, you can change the blink frequency of LED */
-        vTaskDelay(pdMS_TO_TICKS(EXAMPLE_LED_DIM_DELAY_MS));
+    ESP_LOGI(TAG, "Begin scaling");
+    while (1)
+    {
+        int8_t sdm_density = get_default_sdm_density(motor_dir);
+        ESP_LOGI(TAG, "Start at %i", sdm_density);
+        ESP_ERROR_CHECK(sdm_channel_set_pulse_density(sdm_chan, sdm_density));
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        ESP_LOGI(TAG, "Pause");
+        ESP_ERROR_CHECK(sdm_channel_set_pulse_density(sdm_chan, SDM_DUTY_OFF(motor_dir)));
+        vTaskDelay(pdMS_TO_TICKS(2000));
 
-        duty += step;
-        if (duty == EXAMPLE_LED_DIM_DUTY_MAX || duty == EXAMPLE_LED_DIM_DUTY_MIN) {
-            step *= -1;
-        }
+        ESP_LOGI(TAG, "Flipping");
+        motor_dir = !motor_dir;
+        gpio_set_level(DIR_GPIO_NUM,motor_dir);
+        ESP_ERROR_CHECK(sdm_channel_set_pulse_density(sdm_chan, SDM_DUTY_OFF(motor_dir)));
+
     }
 }
